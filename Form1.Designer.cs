b3c4d411805fc5161e740def628d40253ebca2d8
Сергей <serega.iva@outlook.com>
2021-01-10 12:55:50 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace FuelConsumption
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox_distance = new System.Windows.Forms.TextBox();
+			this.textBox_average = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox_cost = new System.Windows.Forms.TextBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label_Tfuel = new System.Windows.Forms.Label();
+			this.label_cost = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.label11 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(177, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Стоимость поездки";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label2.Location = new System.Drawing.Point(13, 29);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(85, 16);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Расстояние";
+			// 
+			// textBox_distance
+			// 
+			this.textBox_distance.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox_distance.Location = new System.Drawing.Point(16, 48);
+			this.textBox_distance.Name = "textBox_distance";
+			this.textBox_distance.Size = new System.Drawing.Size(173, 21);
+			this.textBox_distance.TabIndex = 2;
+			// 
+			// textBox_average
+			// 
+			this.textBox_average.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox_average.Location = new System.Drawing.Point(16, 94);
+			this.textBox_average.Name = "textBox_average";
+			this.textBox_average.Size = new System.Drawing.Size(173, 21);
+			this.textBox_average.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label3.Location = new System.Drawing.Point(13, 75);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(171, 16);
+			this.label3.TabIndex = 7;
+			this.label3.Text = "Средний расход топлива";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label4.Location = new System.Drawing.Point(13, 117);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(160, 16);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Стоимость 1 л. топлива";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label5.Location = new System.Drawing.Point(13, 223);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(235, 16);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "Необходимое количество топлива";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label6.Location = new System.Drawing.Point(13, 269);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(136, 16);
+			this.label6.TabIndex = 10;
+			this.label6.Text = "Стоимость поездки";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.Red;
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.button1.ForeColor = System.Drawing.Color.White;
+			this.button1.Location = new System.Drawing.Point(16, 173);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(122, 36);
+			this.button1.TabIndex = 11;
+			this.button1.Text = "Рассчитать";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox_cost
+			// 
+			this.textBox_cost.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.textBox_cost.Location = new System.Drawing.Point(16, 136);
+			this.textBox_cost.Name = "textBox_cost";
+			this.textBox_cost.Size = new System.Drawing.Size(173, 21);
+			this.textBox_cost.TabIndex = 12;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "км",
+            "мили"});
+			this.comboBox1.Location = new System.Drawing.Point(195, 48);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(64, 21);
+			this.comboBox1.TabIndex = 14;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Items.AddRange(new object[] {
+            "грн",
+            "рубль",
+            "доллар",
+            "евро"});
+			this.comboBox2.Location = new System.Drawing.Point(195, 136);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(64, 21);
+			this.comboBox2.TabIndex = 15;
+			this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label7.Location = new System.Drawing.Point(199, 99);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(60, 16);
+			this.label7.TabIndex = 16;
+			this.label7.Text = "л/100 км";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label8.Location = new System.Drawing.Point(195, 289);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(0, 20);
+			this.label8.TabIndex = 17;
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label9.Location = new System.Drawing.Point(195, 244);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(23, 20);
+			this.label9.TabIndex = 18;
+			this.label9.Text = "л.";
+			// 
+			// label_Tfuel
+			// 
+			this.label_Tfuel.BackColor = System.Drawing.Color.White;
+			this.label_Tfuel.Location = new System.Drawing.Point(16, 243);
+			this.label_Tfuel.Name = "label_Tfuel";
+			this.label_Tfuel.Size = new System.Drawing.Size(173, 21);
+			this.label_Tfuel.TabIndex = 19;
+			// 
+			// label_cost
+			// 
+			this.label_cost.BackColor = System.Drawing.Color.White;
+			this.label_cost.Location = new System.Drawing.Point(16, 288);
+			this.label_cost.Name = "label_cost";
+			this.label_cost.Size = new System.Drawing.Size(173, 21);
+			this.label_cost.TabIndex = 20;
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label10.Location = new System.Drawing.Point(200, 30);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(59, 15);
+			this.label10.TabIndex = 21;
+			this.label10.Text = "Км/Мили";
+			// 
+			// label11
+			// 
+			this.label11.AutoSize = true;
+			this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+			this.label11.Location = new System.Drawing.Point(200, 118);
+			this.label11.Name = "label11";
+			this.label11.Size = new System.Drawing.Size(52, 15);
+			this.label11.TabIndex = 22;
+			this.label11.Text = "Валюта";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.ControlLight;
+			this.ClientSize = new System.Drawing.Size(309, 363);
+			this.Controls.Add(this.label11);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label_cost);
+			this.Controls.Add(this.label_Tfuel);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.textBox_cost);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox_average);
+			this.Controls.Add(this.textBox_distance);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox_distance;
+		private System.Windows.Forms.TextBox textBox_average;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox_cost;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label_Tfuel;
+		private System.Windows.Forms.Label label_cost;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.Label label11;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FuelConsumption
+{
+    public partial class Form1 : Form
+    {
+		double distance;
+		double average_fuel;
+		double cost;
+		double[] ue = new double[] { 1, 0.33, 28, 32 };
+		string[] currency = new string[] { "грн.", "руб.", "дол.", "евр." };
+		int ue_index=-1;
+		int dist_index = -1;
+		public Form1()
+        {
+            InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			string temp = textBox_distance.Text;
+			if (!double.TryParse(temp, out distance))
+				textBox_distance.Text = "";
+			temp = textBox_average.Text;
+			if (!double.TryParse(temp, out average_fuel))
+				textBox_average.Text = "";
+			if (!double.TryParse(temp, out cost))
+				textBox_cost.Text = "";
+			if (ue_index != -1&&dist_index!=-1)
+			    Total();
+			
+		}
+		private void Total()
+		{
+			if (dist_index != 0) distance *= 1.6;
+				double total_fuel = (distance / 100) * average_fuel;
+				label_Tfuel.Text = total_fuel.ToString("N2");
+				label_cost.Text = ((total_fuel * cost) / ue[ue_index]).ToString("N2");
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			dist_index = comboBox1.SelectedIndex;
+		}
+
+		private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			ue_index = comboBox2.SelectedIndex;
+			label8.Text = currency[ue_index];
+		}
+	}
+}
